e189d173b03706419a5eb93c8b1868b24f3eb1ea
nfb1799 <nfb1799@rit.edu>
2021-12-05 18:58:00 -0500

    fixed circleci failure

--- server/controllers/Task.js
+++ server/controllers/Task.js
@@ -61,17 +61,18 @@ const getTasks = (request, response) => {
 const deleteTask = (request, response) => {
   const req = request;
   const res = response;
-  console.log("indeletetask");
+  console.log('indeletetask');
   console.log(req.body);
 
-  return Task.TaskModel.deleteTask(req.session.account._id, req.body.name, req.body.description, (err, docs) => {
-    if (err) {
-      console.log(err);
-      return res.status(400).json({ error: 'An error occurred' });
-    }
+  return Task.TaskModel.deleteTask(req.session.account._id, req.body.name, req.body.description,
+    (err, docs) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).json({ error: 'An error occurred' });
+      }
 
-    return res.json({ tasks: docs });
-  });
+      return res.json({ tasks: docs });
+    });
 };
 
 module.exports.makerPage = makerPage;
